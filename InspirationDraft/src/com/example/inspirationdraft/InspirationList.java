9272a22db30de03f9bc19f582dfdc33e66eee726
Brett David Roads <brett.roads@gmail.com>
2012-11-12 23:34:03 -0700

    InspirationList retooled to mirror Prof Anderson's ProfileList example

--- InspirationDraft/src/com/example/inspirationdraft/InspirationList.java
+++ InspirationDraft/src/com/example/inspirationdraft/InspirationList.java
@@ -1,21 +1,102 @@
 package com.example.inspirationdraft;
 
-import android.util.SparseArray;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-public class InspirationList {
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
-	private SparseArray<Inspiration> inspirationMap = new SparseArray<Inspiration>();
+import android.util.Log;
+
+public class InspirationList implements Iterable<Integer> {
+
+	private Map<Integer,Inspiration> inspirations;
 	
-	public Inspiration getInspiration(int inspirationID) {
-		return inspirationMap.get(inspirationID);
+	public InspirationList() {
+		inspirations = new TreeMap<Integer,Inspiration>();
+	}
+	
+	public void addInspiration(Integer id) {
+		inspirations.put(id, new Inspiration());
+	}
+	
+	public void addInspiration(Integer id, Inspiration data) {
+		inspirations.put(id, data);
+	}
+	
+	public void clear() {
+		inspirations.clear();
+	}
+	
+	public boolean containsID(Integer id) {
+		return inspirations.keySet().contains(id);
+	}
+	
+	public void removeID(Integer id) {
+		inspirations.remove(id);
+	}
+	
+	public Inspiration getID(Integer id) {
+		return inspirations.get(id);
+	}
+	
+	public Iterator<Integer> iterator() {
+		return inspirations.keySet().iterator();
 	}
 
-	public void putInspiration(Inspiration newInspiration) {
-		inspirationMap.put(newInspiration.getID(), newInspiration);
+	public void save(File f) {
+		try {
+			ObjectOutputStream output =
+					new ObjectOutputStream(
+							new FileOutputStream(f));
+			
+			output.writeObject(inspirations);
+			
+			output.close();
+			
+			Log.i("Inspiration Viewer", "Inspirations saved to " + f);
+			
+		} catch (FileNotFoundException ex) {
+			Log.i("Inspiration Viewer", "Error:  File Can Not Be Created: " + f);
+			Log.i("Inspiration Viewer", "Attempt to save inspirations aborted.");
+		} catch (IOException ex) {
+			Log.i("Inspiration Viewer", "Fatal error while saving inspirations.");
+			Log.i("Inspiration Viewer", "Attempt to save profiles aborted.");
+		}
 	}
 	
-	public void removeInspiration(int inspirationID) {
-		inspirationMap.remove(inspirationID);
+	@SuppressWarnings("unchecked")
+	public void load(File f) {
+		try {
+			ObjectInputStream input = 
+					new ObjectInputStream(
+							new FileInputStream(f));
+			
+			inspirations = (TreeMap<Integer,Inspiration>) input.readObject();
+			
+			input.close();
+			
+			Log.i("Inspiration Viewer", "Inspirations loaded from " + f);
+		
+		} catch (FileNotFoundException ex) {
+			Log.i("Inspiration Viewer", "File Not Found: " + f);
+			Log.i("Inspiration Viewer", "Creating an empty set of inspirations.");
+			inspirations.clear();
+		} catch (IOException ex) {
+			Log.i("Inspiration Viewer", "Fatal error while loading inspirations.");
+			Log.i("Inspiration Viewer", "Creating an empty set of inspirations.");
+			inspirations.clear();
+		} catch (ClassNotFoundException ex) {
+			Log.i("Inspiration Viewer", "Fatal error while loading inspirations.");
+			Log.i("Inspiration Viewer", "Creating an empty set of inspirations.");
+			inspirations.clear();
+		}
 	}
 	
 }
