c47a0de3c145ede9d90dfb4b266daddbe03dfb62
Brett David Roads <brett.roads@gmail.com>
2012-11-12 23:49:48 -0700

    Inspiration retooled to mirror Prof Anderson's ProfileData class
    has read write methods, but they have not been tested

--- InspirationDraft/src/com/example/inspirationdraft/Inspiration.java
+++ InspirationDraft/src/com/example/inspirationdraft/Inspiration.java
@@ -1,9 +1,23 @@
 package com.example.inspirationdraft;
 
-public class Inspiration {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class Inspiration implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
 	static int inspirationCount;
 	private int inspirationID;
 	private String content;
+	
+	public Inspiration() {
+		this.inspirationID = inspirationCount;
+		inspirationCount++;
+		this.content = "A man who does not read good books has no advantage over the man who can't read them. -Mark Twain";
+	}
 	
 	public Inspiration(String content){
 		this.inspirationID = inspirationCount;
@@ -12,10 +26,24 @@ public class Inspiration {
 	}
 
 	public int getID() {
-		return inspirationID;
+		return this.inspirationID;
 	}
 	
 	public String getContent() {
-		return content;
+		return this.content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	private void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException {
+		this.inspirationID = (Integer) stream.readObject();
+		this.content = (String) stream.readObject();
+	}
+	
+	private void writeObject(ObjectOutputStream stream) throws IOException {
+		stream.writeObject(this.inspirationID);
+		stream.writeObject(this.content);
 	}
 }
